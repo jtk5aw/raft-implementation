61ad1b14515cf3de4081a3ef85f18f35809b67d5
Jackson Kennedy <jacksontkennedy99@gmail.com>
2024-01-03 00:03:43 -0500

    Tried to set up otel but failed
    Failed is a strong word but it didnt' reaaaaally work.
    Didn't provide any useful info that the regular logs didn't
    so I'm just going back to raw logs for now.

--- raft/risdb/src/server.rs
+++ raft/risdb/src/server.rs
@@ -1,6 +1,8 @@
 use std::env;
 
+use opentelemetry::global;
 use raft_grpc::server::{RisDb, RisDbSetup};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 
 #[tokio::main]
@@ -25,21 +27,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn setup_tracing() -> Result<(), Box<dyn std::error::Error>> {
-    // construct a subscriber that prints formatted traces to stdout
-    let subscriber = tracing_subscriber::fmt()
-        // Use a more compact, abbreviated log format
-        .compact()
-        // Display source code file paths
-        .with_file(true)
-        // Display source code line numbers
-        .with_line_number(true)
-        // Display the thread ID an event was recorded on
-        .with_thread_ids(false)
-        // Don't display the event's target (module path)
-        .with_target(false)
-        // Build the subscriber
-        .finish();
-    // use that subscriber to process traces emitted after this point
-    tracing::subscriber::set_global_default(subscriber)?;
+    // // construct a subscriber that prints formatted traces to stdout
+    // let subscriber = tracing_subscriber::fmt()
+    //     // Use a more compact, abbreviated log format
+    //     .compact()
+    //     // Display source code file paths
+    //     .with_file(true)
+    //     // Display source code line numbers
+    //     .with_line_number(true)
+    //     // Display the thread ID an event was recorded on
+    //     .with_thread_ids(false)
+    //     // Don't display the event's target (module path)
+    //     .with_target(false)
+    //     // Build the subscriber
+    //     .finish();
+    // // use that subscriber to process traces emitted after this point
+    // tracing::subscriber::set_global_default(subscriber)?;
+
+    // Allows you to pass along context (i.e., trace IDs) across services
+    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    // Sets up the machinery needed to export data to Jaeger
+    // There are other OTel crates that provide pipelines for the vendors
+    // mentioned earlier.
+    let tracer = opentelemetry_jaeger::new_pipeline()
+        .with_service_name("risdb")
+        .install_simple()?;
+
+    // Create a tracing layer with the configured tracer
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    // let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
+    //     .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info"));
+
+    // The SubscriberExt and SubscriberInitExt traits are needed to extend the
+    // Registry to accept `opentelemetry (the OpenTelemetryLayer type).
+    tracing_subscriber::registry()
+        // .with(env_filter)
+        .with(opentelemetry)
+        // Continue logging to stdout
+        .try_init()?;
+
     Ok(())
 }
